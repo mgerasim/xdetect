11fe95354e52ddc532458e80e729a36da6f60ea1
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-08-05 12:36:36 +1100

    init

--- js/xdetect.js
+++ js/xdetect.js
@@ -1,11 +1,44 @@
 var button_detect_confirm;
 var input_files;
+var input_file01;
+var input_file02;
+var input_file03;
+var input_file04;
+var input_file05;
+var input_file06;
+var input_file07;
+var input_file08;
+var input_file09;
+var input_file10;
+
 function init() {		
 	input_files = document.querySelector('#input_files');
 	button_detect_confirm = document.querySelector('#detect_confirm');
 	button_detect_confirm.onclick = function () {
 		input_files.style.display = 'block';
 	}	
+	input_file01 = document.querySelector('#file01');
+	input_file02 = document.querySelector('#file02');
+	input_file03 = document.querySelector('#file03');
+	input_file04 = document.querySelector('#file04');
+	input_file05 = document.querySelector('#file05');
+	input_file06 = document.querySelector('#file06');
+	input_file07 = document.querySelector('#file07');
+	input_file08 = document.querySelector('#file08');
+	input_file09 = document.querySelector('#file09');
+	input_file10 = document.querySelector('#file10');
+	button_detect_end = document.querySelector('#detect_end');
+	input_file01.onchange = function () {	input_file02.style.display = 'block';	}
+	input_file02.onchange = function () {	input_file03.style.display = 'block';	}
+	input_file03.onchange = function () {	input_file04.style.display = 'block';	}
+	input_file04.onchange = function () {	input_file05.style.display = 'block';	}
+	input_file05.onchange = function () {	input_file06.style.display = 'block';	}
+	input_file06.onchange = function () {	input_file07.style.display = 'block';	}
+	input_file07.onchange = function () {	input_file08.style.display = 'block';	}
+	input_file08.onchange = function () {	input_file09.style.display = 'block';	}
+	input_file09.onchange = function () {	input_file10.style.display = 'block';	}
+	input_file10.onchange = function () {	button_detect_end.style.display = 'block';	}
+	button_detect_end.onclick = function () {		}
 }
     
 window.onload = init;
@@ -24,9 +57,10 @@ window.onload = init;
 			
 				/*
 			  upload(file);
-			  */
+			  
 			  drawOnCanvas(file); 
 			  displayAsImage(file);
+			  */
 			};
 			
 			function upload(file) {
